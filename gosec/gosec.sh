40f75e02532f9d0db39b16b34ea804933695c896
Neil Pathare <pathare@synopsys.com>
2022-01-18 13:26:33 -0500

    Updating script

--- gosec/gosec.sh
+++ gosec/gosec.sh
@@ -4,7 +4,7 @@ echo 'GoSec Runner'
 
 GoURL="https://go.dev/dl/go1.17.6.linux-amd64.tar.gz"
 GoSecURL='https://raw.githubusercontent.com/securego/gosec/master/install.sh'
-GoSecZIP='go1.17.6.linux-amd64.tar.gz'
+GoSecTAR='go1.17.6.linux-amd64.tar.gz'
 
 # Setup Go
 if [ -f "$GoTAR" ]; then
@@ -12,13 +12,14 @@ if [ -f "$GoTAR" ]; then
 else
     echo "$GoTAR does not exist. Downloading: ${GoURL}"
     curl -O $GoURL
-    tar -xvf $GoTAR
+    tar -xzvf- $GoTAR
     mv go /tmp/
     export PATH=$PATH:/tmp/go/bin:$GOPATH/bin
 fi
 
 # Download GoSec
+echo 'Installing GoSec:'
 curl -sfL $GoSecURL | sh -s latest
 
-echo "Running GoSec:"
+echo 'Running GoSec:'
 ./bin/gosec -exclude-dir=go -fmt=json -out=gosec-results.json ./...
